e6c772e9a606ae8f9b452562d5ad358341cfc461
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-01-16 16:19:41 +0900

    3Dサウンドとりあえず

--- EugeneLib/EugeneLib/Source/Sound/Xaudio2/Xa2Sound3DControl.cpp
+++ EugeneLib/EugeneLib/Source/Sound/Xaudio2/Xa2Sound3DControl.cpp
@@ -1,1 +1,41 @@
 #include "Xa2Sound3DControl.h"
+#include <xaudio2.h>
+#include <x3daudio.h>
+#include <vector>
+
+void EugeneLib::Xa2Sound3DControl::Update3DSound(
+	const Vector3& listenerFront, const Vector3& listenerTop, const Vector3& listenerPos, const Vector3& listenerVeclocity,
+	const Vector3& emitterFront, const Vector3& emitterTop, const Vector3& emitterPos, const Vector3& emitterVelocity)
+{
+	X3DAUDIO_EMITTER emitter{};
+	X3DAUDIO_LISTENER listener{};
+	emitter.OrientFront = X3DAUDIO_VECTOR{ emitterFront.x,emitterFront.y,emitterFront.z };
+	emitter.OrientTop = X3DAUDIO_VECTOR{ emitterTop.x,emitterTop.y,emitterTop.z };
+	emitter.Position = X3DAUDIO_VECTOR{ emitterPos.x,emitterPos.y,emitterPos.z };
+	emitter.Velocity = X3DAUDIO_VECTOR{ emitterVelocity.x,emitterVelocity.y,emitterVelocity.z };
+	emitter.ChannelCount = 1;
+	emitter.CurveDistanceScaler = emitter.DopplerScaler = 1.0f;
+
+	listener.OrientFront = X3DAUDIO_VECTOR{ listenerFront.x,listenerFront.y,listenerFront.z };
+	listener.OrientTop = X3DAUDIO_VECTOR{ listenerTop.x,listenerTop.y,listenerTop.z };
+	listener.Position = X3DAUDIO_VECTOR{ listenerPos.x,listenerPos.y,listenerPos.z };
+	listener.Velocity = X3DAUDIO_VECTOR{ listenerVeclocity.x,listenerVeclocity.y,listenerVeclocity.z };
+
+	X3DAUDIO_DSP_SETTINGS dsp{};
+	dsp.SrcChannelCount = inChannel_;
+	dsp.DstChannelCount = outChannel_;
+	std::vector<float> matrix(outChannel_);
+	dsp.pMatrixCoefficients = matrix.data();
+	X3DAudioCalculate(
+		handle_.data(),
+		&listener,
+		&emitter,
+		X3DAUDIO_CALCULATE_MATRIX | X3DAUDIO_CALCULATE_DOPPLER | X3DAUDIO_CALCULATE_LPF_DIRECT | X3DAUDIO_CALCULATE_REVERB,
+		&dsp
+	);
+
+	submix_->SetOutputMatrix(nullptr, inChannel_, outChannel_, &dsp.ReverbLevel);
+	XAUDIO2_FILTER_PARAMETERS filter{ LowPassFilter, 2.0f * std::sin(X3DAUDIO_PI / 6.0f * dsp.LPFDirectCoefficient) };
+	submix_->SetFilterParameters(&filter);
+	
+}
--- EugeneLib/EugeneLib/Source/Sound/Xaudio2/Xa2Sound3DControl.h
+++ EugeneLib/EugeneLib/Source/Sound/Xaudio2/Xa2Sound3DControl.h
@@ -1,10 +1,24 @@
 #pragma once
 #include "../../../Include/Sound/Sound3DControl.h"
+#include <span>
+
+struct IXAudio2;
+struct IXAudio2SubmixVoice;
 
 namespace EugeneLib
 {
-    class Xa2Sound3DControl :
-        public Sound3DControl
-    {
-    };
+	class Xa2Sound3DControl :
+		public Sound3DControl
+	{
+	public:
+		Xa2Sound3DControl(IXAudio2* xaudio2, std::span<std::uint8_t, 20> handle,std::uint16_t outChannel, std::uint16_t inChannel);
+	private:
+		void Update3DSound(
+			const Vector3& listenerFront, const Vector3& listenerTop, const Vector3& listenerPos, const Vector3& listenerVeclocity
+			, const Vector3& emitterFront, const Vector3& emitterTop, const Vector3& emitterPos, const Vector3& emitterVelocity) final;
+
+		std::span<std::uint8_t, 20> handle_;
+
+		IXAudio2SubmixVoice* submix_;
+	};
 }

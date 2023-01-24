a17e746c057f7fc8f354e6ce9c8794a6478f2f83
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-01-24 12:18:03 +0900

    Vector2、Vector3用のformatterを作成

--- EugeneLib/EugeneLib/Include/Common/Debug.h
+++ EugeneLib/EugeneLib/Include/Common/Debug.h
@@ -9,26 +9,95 @@
 
 #define DebugLog(...) (Eugene::Debug::GetInstance().Log(__VA_ARGS__))
 
+/// <summary>
+/// Vector2用std::formatter
+/// </summary>
+/// <typeparam name="CharT"> 文字列の型 </typeparam>
+template<Eugene::ValueC T, class CharT>
+class std::formatter<Eugene::Vector2Tmp<T>, CharT> :
+	public std::formatter<const CharT*>
+{
+public:
+
+	template<class Out>
+	auto format(const Eugene::Vector2Tmp<T>& vec, std::basic_format_context<Out, CharT>& ctx)
+	{
+		if constexpr (std::is_floating_point<T>::value)
+		{
+			// 浮動小数点型の時
+			return std::format_to(ctx.out(), "x={0:f}y={1:f}", vec.x, vec.y);
+		}
+		else if constexpr (std::is_integral<T>::value)
+		{
+			// 整数型の時
+			return std::format_to(ctx.out(), "x={0:d}y={1:d}", vec.x, vec.y);
+		}
+
+		// それ以外
+		return std::format_to(ctx.out(), "x={0:}y={1:}", vec.x, vec.y);
+	}
+};
+
+/// <summary>
+/// Vector3用std::formatter
+/// </summary>
+/// <typeparam name="CharT"> 文字列の型 </typeparam>
+template<Eugene::ValueC T, class CharT>
+class std::formatter<Eugene::Vector3Tmp<T>, CharT> :
+	public std::formatter<const CharT*>
+{
+public:
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="Out"></typeparam>
+	/// <param name="vec"></param>
+	/// <param name="ctx"></param>
+	/// <returns></returns>
+	template<class Out>
+	auto format(const Eugene::Vector3Tmp<T>& vec, std::basic_format_context<Out, CharT>& ctx)
+	{
+		if constexpr (std::is_floating_point<T>::value)
+		{
+			// 浮動小数点型の時
+			return std::format_to(ctx.out(), "x={0:f}y={1:f}z={2:f}", vec.x, vec.y, vec.z);
+		}
+		else if constexpr (std::is_integral<T>::value)
+		{
+			// 整数型の時
+			return std::format_to(ctx.out(), "x={0:d}y={1:d}z={2:d}", vec.x, vec.y, vec.z);
+		}
+
+		// それ以外
+		return std::format_to(ctx.out(), "x={0:}y={1:}z={2:}", vec.x, vec.y, vec.z);
+	}
+};
+
 namespace Eugene
 {
-	template<class T>
-	concept DebugStr = std::same_as<T, std::u8string>;
-
-	template<class T>
-	concept DebugValue = std::floating_point<T> || std::signed_integral<T> || std::unsigned_integral<T>;
-
 
 	class Debug
 	{
 	public:
 		static Debug& GetInstance(void);
 
+		/// <summary>
+		/// std::formatで書式指定した文字列をデバッグ出力する
+		/// </summary>
+		/// <typeparam name="...Args"></typeparam>
+		/// <param name="formatStr"> フォーマット </param>
+		/// <param name="...args"> 引数 </param>
 		template<class ...Args>
 		constexpr void Log(const std::string& formatStr, const Args&... args)
 		{
 			Log(std::format(formatStr, args));
 		}
 
+		/// <summary>
+		/// 文字列をデバッグ出力する
+		/// </summary>
+		/// <param name="str"></param>
 		void Log(const std::string& str);
 
 	private:

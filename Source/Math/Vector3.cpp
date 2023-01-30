13f7168badba8ae33de17fb9f8073c5d72841e1e
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-01-30 20:18:34 +0900

    webから

--- Source/Math/Vector3.cpp
+++ Source/Math/Vector3.cpp
@@ -1,6 +1,8 @@
 #include "../../Include/Math/Vector3.h"
 
-void Eugene::Vector3::Normalize(void)
-{
-
-}
+// 明示的インスタンス
+template Eugene::Vector3Tmp<std::uint16_t>;
+template Eugene::Vector3Tmp<float>;
+template Eugene::Vector3Tmp<double>;
+template Eigene::Vector3Tmp<std::int32_t>;
+template Eugene::Vector3Tmp<std::int64_t>;

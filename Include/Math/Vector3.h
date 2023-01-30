8d5399d87984d56db0a49317c660441095d8ca5b
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-01-31 00:58:17 +0900

    Vector3を明示的インスタンス化

--- Include/Math/Vector3.h
+++ Include/Math/Vector3.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cmath>
 #include "MathConcepts.h"
+//#include "../../Include/ThirdParty/DirectXMath/DirectXMath.h"
 
 namespace Eugene
 {
@@ -112,14 +113,8 @@ namespace Eugene
 	template<ValueC T>
 	constexpr Vector3Tmp<T> operator-(const Vector3Tmp<T>& lVec, const Vector3Tmp<T>& rVec)
 	{
-		/*if constexpr (std::is_floating_point<T>())
 		{
-			auto result = DirectX::operator-(lVec.ToSimdVector(), rVec.ToSimdVector());
-			return { result.m128_f32[0], result.m128_f32[1], result.m128_f32[2] };
-		}
-		else*/
-		{
-			return { lVec.x - rVec.x, lVec.y - rVec.y , lVec.z - rVec.z };
+			return { static_cast<T>(lVec.x - rVec.x), static_cast<T>(lVec.y - rVec.y) , static_cast<T>(lVec.z - rVec.z) };
 		}
 	}
 
@@ -154,14 +149,8 @@ namespace Eugene
 	template<ValueC T>
 	constexpr Vector3Tmp<T> operator*(const Vector3Tmp<T>& lVec, const T& val)
 	{
-	/*	if constexpr (std::is_floating_point<T>())
 		{
-			auto result = DirectX::operator*(lVec.ToSimdVector(), DirectX::XMVectorSet(val, val, val, 0));
-			return { result.m128_f32[0], result.m128_f32[1], result.m128_f32[2] };
-		}*/
-		//else
-		{
-			return { lVec.x * val, lVec.y * val, lVec.z * val };
+			return { static_cast<T>(lVec.x * val), static_cast<T>(lVec.y * val), static_cast<T>(lVec.z * val) };
 		}
 	}
 
@@ -244,179 +233,7 @@ namespace Eugene
 		return lVec.x == rVec.x && lVec.y == rVec.y && lVec.z == rVec.z;
 	}
 
-
-	template<ValueC T>
-	inline T Vector3Tmp<T>::Magnitude(void) const
-	{
-		/*if constexpr (std::is_floating_point<T>())
-		{
-			return DirectX::XMVector3Length(DirectX::XMVectorSet(x, y, z, 0)).m128_f32[0];
-		}*/
-		//else
-		{
-			return std::sqrt(x * x + y * y + z * z);
-		}
-
-	}
-
-	template<ValueC T>
-	inline T Vector3Tmp<T>::SqMagnitude(void) const
-	{
-		/*if constexpr (std::is_floating_point<T>())
-		{
-			return DirectX::XMVector3LengthSq(DirectX::XMVectorSet(x, y, z, 0)).m128_f32[0];
-		}*/
-		//else
-		{
-			return x * x + y * y + z * z;
-		}
-	}
-
-	template<ValueC T>
-	inline void Vector3Tmp<T>::Normalize(void)
-	{
-		T mag = Magnitude();
-		if (mag == static_cast<T>(0))
-		{
-			return;
-		}
-		x /= mag;
-		y /= mag;
-		z /= mag;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T> Vector3Tmp<T>::Normalized(void) const
-	{
-		T mag = Magnitude();
-		return { x / mag, y / mag , z / mag};
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T> Vector3Tmp<T>::Refrect(const Vector3Tmp<T>& n)
-	{
-		return (*this) - (n * (static_cast<T>(2) * (x * n.x + y * n.y + z * n.z)));
-	}
-
-
-
-	template<ValueC T>
-	inline Vector3Tmp<T> Vector3Tmp<T>::operator-() const
-	{
-		return { -x,-y , -z};
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T> Vector3Tmp<T>::operator+() const
-	{
-		return { +x,+y, +z };
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator+=(const Vector3Tmp<T>& vec)
-	{
-		x += vec.x;
-		y += vec.y;
-		z += vec.z;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator+=(const T& val)
-	{
-		x += val;
-		y += val;
-		z += val;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator-=(const Vector3Tmp<T>& vec)
-	{
-		x -= vec.x;
-		y -= vec.y;
-		z -= vec.z;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator-=(const T& val)
-	{
-		x -= val;
-		y -= val;
-		z -= val;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator*=(const Vector3Tmp<T>& vec)
-	{
-		x *= vec.x;
-		y *= vec.y;
-		z *= vec.z;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator*=(const T& val)
-	{
-		x *= val;
-		y *= val;
-		z *= val;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator/=(const Vector3Tmp<T>& vec)
-	{
-		if (vec.x == static_cast<T>(0) || vec.y == static_cast<T>(0) || vec.z == static_cast<T>(0))
-		{
-			return *this;
-		}
-		x /= vec.x;
-		y /= vec.y;
-		z /= vec.z;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator/=(const T& val)
-	{
-		if (val == static_cast<T>(0))
-		{
-			return *this;
-		}
-		x /= val;
-		y /= val;
-		z /= val;
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator%=(const Vector3Tmp<T>& vec)
-	{
-		if (vec.x == static_cast<T>(0) || vec.y == static_cast<T>(0) || vec.z == static_cast<T>(0))
-		{
-			return *this;
-		}
-		x = static_cast<T>(std::fmod(x, vec, x));
-		y = static_cast<T>(std::fmod(y, vec.y));
-		z = static_cast<T>(std::fmod(z, vec.z));
-		return *this;
-	}
-
-	template<ValueC T>
-	inline Vector3Tmp<T>& Vector3Tmp<T>::operator%=(const T& val)
-	{
-		if (val == static_cast<T>(0))
-		{
-			return *this;
-		}
-		x = static_cast<T>(std::fmod(x, val));
-		y = static_cast<T>(std::fmod(y, val));
-		z = static_cast<T>(std::fmod(z, val));
-		return *this;
-	}
+	
 
 	
 

0da438edde07ae8dfc0fa6efeedd8633437ddb92
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-02-07 14:38:21 +0900

    サンプラー途中

--- Include/Graphics/Sampler.h
+++ Include/Graphics/Sampler.h
@@ -2,6 +2,36 @@
 
 namespace Eugene
 {
+	enum class TextureAddressMode
+	{
+		Wrap = 1,
+		Mirror = 2,
+		Clamp = 3,
+		Border = 4,
+		MirrorOnce = 5
+	};
+
+	enum class SampleFilter
+	{
+		Point = 0,
+		Linear = 0x15,
+		Anisotropic = 0x55
+	};
+
+	struct SamplerLayout
+	{
+		SamplerLayout(
+			TextureAddressMode u = TextureAddressMode::Wrap,
+			TextureAddressMode v = TextureAddressMode::Wrap,
+			TextureAddressMode w = TextureAddressMode::Wrap,
+			SampleFilter filter = SampleFilter::Point
+		);
+		TextureAddressMode u_;
+		TextureAddressMode v_;
+		TextureAddressMode w_;
+		SampleFilter filter_;
+	};
+
 	class Sampler
 	{
 	public:

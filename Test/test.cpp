bdf096fbc6453d70a79b93acdbc90e85a5c7dfd7
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-03-13 23:08:34 +0900

    cmakeファイル変更

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,9 +1,9 @@
 #include "Catch2/src/catch2/catch_test_macros.hpp""
-#include <Eugene.h>
+#include <EugeneLib.h>
 #pragma comment(lib,"EugeneLib.lib")
 
 TEST_CASE("CreateWindow", "[function]")
 {
-	Eugene::System* window = CreateSystem({ 1280.0f, 720.0f }, u8"test");
+	Eugene::System* window = Eugene::CreateSystem({ 1280.0f, 720.0f }, u8"test");
 	delete window;
 }

03e21690e1313942cdc411725cda3dd6f903c56b
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-04-27 16:41:23 +0900

    初期値を修正

--- Include/Math/Quaternion.h
+++ Include/Math/Quaternion.h
@@ -13,7 +13,7 @@ namespace Eugene
 	{
 	public:
 		constexpr Quaternion() :
-			x{0.0f}, y{0.0f}, z{0.0f}, w{0.0f}
+			x{0.0f}, y{0.0f}, z{0.0f}, w{1.0f}
 		{}
 
 		constexpr Quaternion(float inX, float inY, float inZ, float inW):
